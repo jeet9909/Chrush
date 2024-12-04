ea0c078cbdc66918b9c28c62ff4d049cb34eb2a5
unknown <gajerajeet88@gmail.com>
2024-12-04 23:14:57 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>My Rolly</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="container">
+        <!-- Tenor GIF Embed -->
+        <div class="tenor-gif-embed" 
+            data-postid="14607796449572371406" 
+            data-share-method="host" 
+            data-aspect-ratio="1.26396" 
+            data-width="100%">
+            <a href="https://tenor.com/view/squidward-spare-change-spare-some-change-begging-poor-gif-14607796449572371406">Squidward Spare Change GIF</a> from 
+            <a href="https://tenor.com/search/squidward-gifs">Squidward GIFs</a>
+        </div>
+        <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+        <!-- Heading and Paragraph -->
+        <h1>piliz meri golu molu Rolly Polly 4 hr Ghnte lge hai </h1>
+        <p>Wrna guddi laal krunga marna hai kya dar nhi lgta?</p>
+
+        <!-- Buttons -->
+        <div class="btn">
+            <a href="yes.html">Yes</a>
+            <a href="no3.html" id="move-random">No</a>
+        </div>
+    </div> 
+
+    <!-- Link to External JavaScript File -->
+    <script src="script.js"></script>
+</body>
+</html>
